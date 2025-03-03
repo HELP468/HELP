298a22cff3416f81e96bc8b1a1974fa6eb380c73
Andrew Lebo <andrewlebo2@gmail.com>
2025-03-03 10:33:10 -0500

    CRLF -> LF

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@ echo on
 docker image rm -f db:latest
 docker image rm -f server:latest
 docker image rm -f mq:latest
-docker build -t db:latest ./docker/db
-docker build -t mq:latest ./docker/mq
-docker build -t server:latest ./docker/server
+docker build --no-cache -t db:latest ./docker/db
+docker build --no-cache -t mq:latest ./docker/mq
+docker build --no-cache -t server:latest ./docker/server
