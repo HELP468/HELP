468709ba0befcc8b07a286c1065f2545b155ee70
Drew Lebo <andrewlebo2@gmail.com>
2025-03-26 09:15:51 -0400

    Sped up build process

--- build.bat
+++ build.bat
@@ -1,7 +1,5 @@
 echo on
 docker image rm -f db:latest
-docker image rm -f server:latest
 docker image rm -f mq:latest
-docker build --no-cache -t db:latest ./docker/db
-docker build --no-cache -t mq:latest ./docker/mq
-docker build --no-cache -t server:latest ./docker/server
+docker image rm -f server:latest 
+docker compose -f .\docker-compose.yml up -d --build
--- build.sh
+++ build.sh
@@ -6,6 +6,4 @@ docker image rm -f db:latest || true
 docker image rm -f mq:latest || true
 docker image rm -f server:latest || true
 
-docker build -t db:latest ./docker/db
-docker build -t mq:latest ./docker/mq
-docker build -t server:latest ./docker/server
+docker compose -f .\docker-compose.yml up -d --build
