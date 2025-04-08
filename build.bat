2e8670e6ede38e0e0fb3519fbd3bf2c8c322a300
Drew Lebo <andrewlebo2@gmail.com>
2025-04-07 22:22:58 -0400

    simplified build scripts

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@ docker image rm -f db:latest
 docker image rm -f mq:latest
 docker image rm -f frontend:latest
 docker image rm -f backend:latest 
-docker compose -f .\docker-compose.yml up -d --build
+docker compose up -d --build
