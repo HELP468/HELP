91607790e2768ecb3d401045925cb0cf082ddf04
uayub02 <UA1031957@wcupa.edu>
2025-04-09 10:15:58 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ export $(grep -v '^#' .env | xargs)
 echo "$GHCR_PAT" | docker login ghcr.io -u help468 --password-stdin
 
 # Remove old images for a clean rebuild
-docker image rm -f ghcr.io/help468/help/backend_server:dev || true
-docker image rm -f temporalcrow/frontend_server:dev || true
+docker image rm -f ghcr.io/help468/help/backend_server:latest || true
+docker image rm -f temporalcrow/frontend_server:latest || true
 
 # Build up the stack
-docker compose -f docker-compose.dev.yml up -d --build
+docker compose -f docker-compose.prod.yml up -d --build
