2746e517a6d3961b245410e8d6f4c99724a50174
Drew Lebo <andrewlebo2@gmail.com>
2025-04-09 10:06:35 -0400

    Test Push

--- build.bat
+++ build.bat
@@ -1,3 +1,14 @@
-echo on
+@echo off
 
-docker compose up -d --build
+REM Load .env variables
+for /f "delims=" %%a in (.env) do set %%a
+
+REM Login to GHCR
+echo %GHCR_PAT% | docker login ghcr.io -u help468 --password-stdin
+
+REM Remove old image
+docker image rm -f ghcr.io/help468/help/backend_server:dev
+docker image rm -f temporalcrow/frontend_server:dev
+
+REM Start up the stack
+docker compose -f docker-compose.dev.yml up -d --build
--- build.sh
+++ build.sh
@@ -2,9 +2,15 @@
 
 set -e
 
-docker image rm -f db:latest || true
-docker image rm -f mq:latest || true
-docker image rm -f frontend:latest || true
-docker image rm -f backend:latest || true
+# Load .env variables
+export $(grep -v '^#' .env | xargs)
 
-docker compose up -d --build
+# Login to GHCR
+echo "$GHCR_PAT" | docker login ghcr.io -u help468 --password-stdin
+
+# Remove old images for a clean rebuild
+docker image rm -f ghcr.io/help468/help/backend_server:dev || true
+docker image rm -f temporalcrow/frontend_server:dev || true
+
+# Build up the stack
+docker compose -f docker-compose.dev.yml up -d --build
