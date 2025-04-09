73c796f49d4dffb0a2f09b564df0385f45700e05
uayub02 <UA1031957@wcupa.edu>
2025-04-09 10:15:14 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -7,8 +7,8 @@ REM Login to GHCR
 echo %GHCR_PAT% | docker login ghcr.io -u help468 --password-stdin
 
 REM Remove old image
-docker image rm -f ghcr.io/help468/help/backend_server:dev
-docker image rm -f temporalcrow/frontend_server:dev
+docker image rm -f ghcr.io/help468/help/backend_server:latest
+docker image rm -f temporalcrow/frontend_server:latest
 
 REM Start up the stack
-docker compose -f docker-compose.dev.yml up -d --build
+docker compose -f docker-compose.prod.yml up -d --build
