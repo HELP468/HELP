f2f45f4731f10e4e08356113b2b380db547a3007
Drew Lebo <andrewlebo2@gmail.com>
2025-04-18 09:17:58 -0400

    quick entrypoint fix

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -6,9 +6,6 @@ echo "Using SERVER_NAME=$SERVER_NAME"
 envsubst '${SERVER_NAME}' < /etc/nginx/conf.d/nginx.http.conf > /etc/nginx/conf.d/default.conf
 
 # Conditionally include HTTPS config if certs exist
-CERT_PATH="/etc/letsencrypt/live/${SERVER_NAME}/fullchain.pem"
-KEY_PATH="/etc/letsencrypt/live/${SERVER_NAME}/privkey.pem"
-
 if [ -f "$CERT_PATH" ] && [ -f "$KEY_PATH" ]; then
     echo "Found SSL certs for ${SERVER_NAME}, enabling HTTPS"
     envsubst '${SERVER_NAME}' < /etc/nginx/conf.d/nginx.https.conf >> /etc/nginx/conf.d/default.conf
