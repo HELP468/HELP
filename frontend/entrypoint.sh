d6cf6fb903a9a96f6b04fe8e6ec4c60cfcdf65d7
Drew Lebo <andrewlebo2@gmail.com>
2025-04-18 10:08:33 -0400

    Cleanup

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -8,16 +8,12 @@ envsubst '${SERVER_NAME}' < /etc/nginx/conf.d/nginx.http.conf > /etc/nginx/conf.
 # Debugging: Check if substitution worked for HTTP config
 cat /etc/nginx/conf.d/default.conf
 
-<<<<<<< HEAD
-# Conditionally include HTTPS config if certs exist
-=======
 CERT_PATH="/etc/letsencrypt/live/${SERVER_NAME}/fullchain.pem"
 KEY_PATH="/etc/letsencrypt/live/${SERVER_NAME}/privkey.pem"
 
 echo "Checking cert path: $CERT_PATH"
 echo "Checking key path: $KEY_PATH"
 
->>>>>>> dev
 if [ -f "$CERT_PATH" ] && [ -f "$KEY_PATH" ]; then
     echo "Found SSL certs for ${SERVER_NAME}, enabling HTTPS"
     envsubst '${SERVER_NAME}' < /etc/nginx/conf.d/nginx.https.conf > /etc/nginx/conf.d/default.conf
