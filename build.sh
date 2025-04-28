b9f7117ecab8c1e999e3ba7e9def7fd5daa67a27
uaayub <ayubumer79@gmail.com>
2025-04-28 09:10:36 -0400

    fix running on mac

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -e
 # Load .env variables
 export $(grep -v '^#' .env | xargs)
 
+# Login to GHCR
+echo "$GHCR_PAT" | docker login ghcr.io -u "$GHCR_USER" --password-stdin
+
 # Remove old images for a clean rebuild
 docker image rm -f temporalcrow/backend_server:latest || true
 docker image rm -f temporalcrow/frontend_server:latest || true
